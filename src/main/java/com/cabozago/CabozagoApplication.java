03f4deb9a1770427b10705082cf764072f4eea53
DESKTOP-BBQAPPJ\user <wltjs188@gmail.com>
2022-02-14 15:07:20 +0900

    init

---
+++ src/main/java/com/cabozago/CabozagoApplication.java
@@ -0,0 +1,13 @@
+package com.cabozago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabozagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabozagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cabozago/CabozagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabozago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabozagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
